7e38f391c4fe210aa6c4830bd8b8cc90547503ff
vnqmai <vnqmai01@gmail.com>
2020-01-19 15:44:03 +0700

    move navbar.jsx to component/header folder

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import logo from './logo.svg';
 // import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
-import { NavbarComponent } from './components/navbar';
+import { NavbarComponent } from './components/header/navbar';
 import './public/css/style.css';
 
 function App() {
